2dd0140c35a609cadfd0a5178ba091bb1431edf8
Kishore Kumar <abyu.kishore@gmail.com>
2011-07-11 22:44:25 +0530

    Added Cucumber steps

--- src/test/cucumber/features/get_rover_postion.feature
+++ src/test/cucumber/features/get_rover_postion.feature
@@ -1,5 +1,4 @@
 Feature: Get Rover Position
     Scenario: Get the position of the rover for given input
-        Given I start my application
-        When I provide "input1" as my input file
+        Given I start my application with "input1" as my input file
         Then I receive the output as in "output1" file
--- src/test/cucumber/java/marsrover/cucumber/steps/BasicSteps.java
+++ src/test/cucumber/java/marsrover/cucumber/steps/BasicSteps.java
@@ -2,6 +2,25 @@ package marsrover.cucumber.steps;
 
 import cuke4duke.annotation.I18n.EN.*;
 
-public class BasicSteps {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
+import static org.junit.matchers.JUnitMatchers.containsString;
+
+public class BasicSteps {
+    private Marsrover marsrover;
+
+    @Given("^I start my application with \"([^\"]*)\" as my input file$")
+    public void startApplication(String inputFile) {
+        marsrover = new Marsrover(inputFile);
+    }
+
+    @Then("^I receive the output as in \"([^\"]*)\" file$")
+    public void checkOutput(String outputFile) throws IOException {
+        FileReader file = new FileReader(outputFile);
+        BufferedReader readFile = new BufferedReader(file);
+        assertThat(marsrover.getOutput(), containsString(readFile.readLine()));
+    }
 }
